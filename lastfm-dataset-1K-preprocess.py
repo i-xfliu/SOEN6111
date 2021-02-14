7610047d8755dae58410aa60c6d1ba87b84c929d
Xiaoyun Liao <sharon.liaoxy@gmail.com>
2021-02-13 22:53:32 -0500

    added dataset The Last.fm Dataset

---
+++ lastfm-dataset-1K-preprocess.py
@@ -0,0 +1,104 @@
+# Spark imports
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+# Dask imports
+# import dask.bag as db
+# import dask.dataframe as df_user_track  # you can use Dask bags or dataframes
+from csv import reader
+import pandas as pd
+import re
+import pyarrow._parquet as _parquet
+
+# userid-profile.tsv
+# lastfm-dataset-1k.snappy.parquet
+# http://ocelma.net/MusicRecommendationDataset/lastfm-1K.html
+# user - 992 rows, ['#id', 'gender', 'age', 'country', 'registered']
+# history -19098862 rows, ['user_id', 'timestamp', 'artist_id', 'artist_name', 'track_id', 'track_name']
+
+#Initialize a spark session.
+def init_spark():
+    spark = SparkSession \
+        .builder \
+        .appName("Python Spark SQL basic example") \
+        .config("spark.some.config.option", "some-value") \
+        .getOrCreate()
+    return spark
+
+spark = init_spark()
+
+df_user_profile = spark.read.csv('data/lastfm-dataset-1k/userid-profile.tsv',sep='\t', header=True)
+print(df_user_profile.count())
+# 992
+print(df_user_profile.columns)
+# ['#id', 'gender', 'age', 'country', 'registered']
+print(df_user_profile.head())
+# Row(#id='user_000001', gender='m', age=None, country='Japan', registered='Aug 13, 2006')
+
+
+
+# parquet
+save_filepath = "data/lastfm-dataset-1k/lastfm-dataset-1k.snappy.parquet"
+# df_user_track.to_parquet(save_filepath, compression='snappy', index=False)
+
+df_user_track = spark.read.parquet(save_filepath)
+df_user_track.write.option("parquet.block.size", 100 * 1024 * 1024)
+df_user_track.write.parquet("data/lastfm-dataset-1k_history")
+
+
+
+# 19098862
+print(df_user_track.count())
+# ['user_id', 'timestamp', 'artist_id', 'artist_name', 'track_id', 'track_name']
+print(df_user_track.columns)
+# Row(
+# user_id='user_000001',
+# timestamp=datetime.datetime(2006, 8, 13, 9, 59, 20),
+# artist_id='09a114d9-7723-4e14-b524-379697f6d2b5',
+# artist_name='Plaid & Bob Jaroc',
+# track_id='c4633ab1-e715-477f-8685-afa5f2058e42',
+# track_name='The Launching Of Big Face')
+print(df_user_track.head())
+print(df_user_track.schema)
+# StructType(
+# List(StructField(user_id,StringType,true),
+# StructField(timestamp,TimestampType,true),
+# StructField(artist_id,StringType,true),
+# StructField(artist_name,StringType,true),
+# StructField(track_id,StringType,true),
+# StructField(track_name,StringType,true)))
+
+user_track_df = df_user_track.groupby('user_id', 'track_id').count()
+# Row(
+# user_id='user_000001',
+# track_id='7b793966-abc8-423d-bddc-1a761213d5f7',
+# count=10)
+print(user_track_df.head())
+
+
+
+
+
+
+
+# loading tsv by pandas, then cover to parquet
+
+# df_user_track = pd.read_csv(
+#     "data/lastfm_userid-timestamp-artid-artname-traid-traname.tsv", sep='\t', header=None,
+#     names=[
+#         'user_id', 'timestamp', 'artist_id', 'artist_name', 'track_id', 'track_name'
+#     ],
+#     # skiprows=[
+#     #     2120260-1, 2446318-1, 11141081-1,
+#     #     11152099-1, 11152402-1, 11882087-1,
+#     #     12902539-1, 12935044-1, 17589539-1
+#     # ]
+#     error_bad_lines= False
+# )
+# df_user_track["timestamp"] = pd.to_datetime(df_user_track.timestamp)
+# df_user_track.sort_values(['user_id', 'timestamp'], ascending=True, inplace=True)
+# print(f'Number of Records: {len(df_user_track):,}\nUnique Users: {df_user_track.user_id.nunique()}\nUnique Artist:{df_user_track.artist_id.nunique():,}')
+#
+
+# save_filepath = "data/lastfm-dataset-1k.snappy.parquet"
+# df_user_track.to_parquet(save_filepath, compression='snappy', index=False)
---
+++ lastfm-subset-jason_preprocess.py
@@ -0,0 +1,88 @@
+import os
+# Spark imports
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+# Dask imports
+# import dask.bag as db
+# import dask.dataframe as df_user_track  # you can use Dask bags or dataframes
+from csv import reader
+import pandas as pd
+import re
+import pyarrow._parquet as _parquet
+
+
+# http://millionsongdataset.com/lastfm/
+# 10k tracks, each one has multiple similar tracks and tags
+# ['artist', 'similars', 'tags', 'timestamp', 'title', 'track_id']
+
+
+def init_spark():
+    spark = SparkSession \
+        .builder \
+        .appName("Python Spark SQL basic example") \
+        .config("spark.some.config.option", "some-value") \
+        .getOrCreate()
+    return spark
+
+
+spark = init_spark()
+
+
+class load_all_json_file(object):
+    file_path_list = []
+    def get_file_path(self, dir_parth):
+        file_list = [f'{dir_parth}/{x.strip()}' for x in os.listdir(dir_parth)]
+        for path in file_list:
+            if path[-4:] == 'json':
+                self.file_path_list.append(path)
+            elif path[-5:] == 'Store':
+                print()
+            else:
+                self.get_file_path(path)
+
+
+# cover json to parquet
+def read_json_to_parquet():
+    loader = load_all_json_file()
+    # json folder has deleted
+    loader.get_file_path("data/lastfm-sub_data/lastfm_subset")
+    all_file_path = loader.file_path_list
+    lastfm_df = spark.read.json(all_file_path)
+    lastfm_df.write.option("parquet.block.size", 10 * 1024 * 1024)
+    lastfm_df.write.parquet("data/track_artist_sml_tag")
+
+
+
+
+# load data from parquet fold
+lastfm_df = spark.read.parquet("data/track_artist_sml_tag/*.parquet")
+
+
+print(lastfm_df.count())
+# 9330
+print(lastfm_df.columns)
+# ['artist', 'similars', 'tags', 'timestamp', 'title', 'track_id']
+print(lastfm_df.head())
+# artist='The Shirelles',
+# similars=[
+# ['TRCCSCE128F92EF9A9', '1'],
+# ['TRCERNU128F92E1921', '1'],
+# ['TRFGOKM128F92F13FF', '1'],
+# ['TRFXYRO128F9311F1F', '1'],
+# ['TRQIIYJ128F92FC318', '1'],
+# ['TRYZQVK128F92FE86E', '0.87287'] ...]
+# tags=[['oldies', '100'], ['60s', '90'] ....],
+# timestamp='2011-09-07 13:30:37.182021',
+# title='Dedicated To the One I Love',
+# track_id='TRAHBWE128F9349247'
+
+print(lastfm_df.schema)
+# StructType(
+# List(StructField(artist,StringType,true),
+# StructField(similars,ArrayType(ArrayType(StringType,true),true),true),
+# StructField(tags,ArrayType(ArrayType(StringType,true),true),true),
+# StructField(timestamp,StringType,true),
+# StructField(title,StringType,true),
+# StructField(track_id,StringType,true))
+# )
